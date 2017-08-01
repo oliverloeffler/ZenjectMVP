a93e286f4fc4875b73b13d7a057d35cd5f3b8ce3
Oliver Löffler <oliver.loeffler@fluffyfairygames.com>
2017-08-01 21:11:09 +0200

    adds reactive command with argument

---
+++ Assets/Scripts/CustomRx/ReactiveCommandExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UniRx;
+
+namespace CustomRx
+{
+    public static class ReactiveCommandExtensions
+    {
+        public static IDisposable BindTo<T>(this ReactiveCommand<T> command, UnityEngine.UI.Button button, T arg)
+        {
+            var d1 = command.CanExecute.SubscribeToInteractable(button);
+            var d2 = button.OnClickAsObservable().SubscribeWithState(command, (_, c) => c.Execute(arg));
+            return StableCompositeDisposable.Create(d1, d2);
+        }
+    }
+}
